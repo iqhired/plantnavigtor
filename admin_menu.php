661e4aaaa96929bad4fb78e35bb8a73742b4c8fe
Asha M S <ashams001@gmail.com>
2022-03-24 19:27:11 +0530

    updated the path

--- admin_menu.php
+++ admin_menu.php
@@ -1686,11 +1686,7 @@ $msg = explode(',', $msg); ?>
 <script src="<?php echo $siteURL; ?>bootstrap/js/bootstrap.min.js"></script>
 <script src="<?php echo $siteURL; ?>plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
 <!--<script src="--><?php //echo $siteURL; ?><!--assets/js/app.js"></script>-->
-<script>
-    $(document).ready(function () {
-        App.init();
-    });
-</script>
+
 <script src="<?php echo $siteURL; ?>assets/js/custom.js"></script>
 <!-- END GLOBAL MANDATORY SCRIPTS -->
 <script src="<?php echo $siteURL; ?>assets/js/dashboard/dash_2.js"></script>
