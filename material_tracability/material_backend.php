ad80cbb1feb115d94755e89cdf22d5d4896c1b40
Asha M S <ashams001@gmail.com>
2022-05-11 12:38:29 +0530

    material trace email

--- material_tracability/material_backend.php
+++ material_tracability/material_backend.php
@@ -1,5 +1,9 @@
 <?php
 include("../config.php");
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+require '../vendor/autoload.php';
 $array = json_decode($_POST['info']);
 $drag_drop_res = (array) json_decode($array);
 
@@ -49,7 +53,7 @@ if(count($_POST)>0) {
 		  $out_of_tolerance_mail_list1 = $rowc05['teams'];
 		  $out_of_tolerance_mail_list_users = $rowc05['users'];
 //	$subject = "Users Mail Report";
-    require '../vendor/autoload.php';
+
 
     //mail code start
     $mail = new PHPMailer();
@@ -64,7 +68,7 @@ if(count($_POST)>0) {
     $mail->setFrom('admin@plantnavigator.com', 'Admin Plantnavigator');
 // mail code over
 //	$message = "This is System generated Mail when out of telerance value added into the form. please go to below link to check the form.";
-    $del_query = sprintf("SELECT part_name ,pn.part_number, line_name ,part_family_name   FROM  material_tracability as mt inner join cam_line as cl on mt.line_no = cl.line_id inner join pm_part_family as pf on mt.part_family_id= pf.pm_part_family_id 
+    $del_query = sprintf("SELECT pn.part_name ,pn.part_number, cl.line_name ,part_family_name   FROM  material_tracability as mt inner join cam_line as cl on mt.line_no = cl.line_id inner join pm_part_family as pf on mt.part_family_id= pf.pm_part_family_id 
 inner join pm_part_number as pn on mt.part_no=pn.pm_part_number_id where mt.material_id='$material_trace_id'");
     $del_query_01 = mysqli_query($db, $del_query);
     $del_query_row = mysqli_fetch_array($del_query_01);
