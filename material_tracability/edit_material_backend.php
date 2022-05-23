d5964de676d926d505f2d3bf2f183d5462750999
Asha M S <ashams001@gmail.com>
2022-05-23 19:22:01 +0530

    email on edit for material traceability

--- material_tracability/edit_material_backend.php
+++ material_tracability/edit_material_backend.php
@@ -90,7 +90,102 @@ if(count($_POST)>0) {
         }
     }
 
+	if ($material_status == '0') {
 
+		$qur05 = mysqli_query($db, "SELECT * FROM `material_config` where material_id = '$material_type' ");
+		$rowc05 = mysqli_fetch_array($qur05);
+		$out_of_tolerance_mail_list1 = $rowc05['teams'];
+		$out_of_tolerance_mail_list_users = $rowc05['users'];
+//	$subject = "Users Mail Report";
+
+
+		//mail code start
+		$mail = new PHPMailer();
+		$mail->isSMTP();
+//$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+		$mail->Host = 'smtp.gmail.com';
+		$mail->Port = 587;
+		$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+		$mail->SMTPAuth = true;
+		$mail->Username = 'admin@plantnavigator.com';
+		$mail->Password = 'S@@rgummi@2021';
+		$mail->setFrom('admin@plantnavigator.com', 'Admin Plantnavigator');
+// mail code over
+//	$message = "This is System generated Mail when out of telerance value added into the form. please go to below link to check the form.";
+		$del_query = sprintf("SELECT pn.part_name ,pn.part_number, cl.line_name ,part_family_name   FROM  material_tracability as mt inner join cam_line as cl on mt.line_no = cl.line_id inner join pm_part_family as pf on mt.part_family_id= pf.pm_part_family_id 
+inner join pm_part_number as pn on mt.part_no=pn.pm_part_number_id where mt.material_id='$material_trace_id'");
+		$del_query_01 = mysqli_query($db, $del_query);
+		$del_query_row = mysqli_fetch_array($del_query_01);
+		$del_user_id = '1';
+//    $del_user_id = $rowc04['created_by'];
+		$del_query_2 = sprintf("SELECT user_name , firstname , lastname from cam_users where users_id='$del_user_id'");
+		$del_query_02 = mysqli_query($db, $del_query_2);
+		$del_query_row_1 = mysqli_fetch_array($del_query_02);
+		$line1 = "There have been issue(s) found with the following material(s):";
+		$line2 = $del_query_row['line_name'];
+		$subject = $line2 . " - Material Rejected";
+//    $form_name = $del_query_row['form_name'];
+		$p_num = $del_query_row['part_number'];
+		$p_name = $del_query_row['part_name'];
+		$pf_name = $del_query_row['part_family_name'];
+		$form_submitted_by = $del_query_row_1['firstname'] . " " . $del_query_row_1['lastname'];
+
+		$message = '<br/><table rules=\"all\" style=\"border-color: #666;\" border=\"1\" cellpadding=\"10\">';
+//    $message .= "<tr><td style='background: #eee;padding: 5px 10px ;'><strong>Form Name : </strong> </td><td>" . $form_name . "</td></tr>";
+		$message .= "<tr><td style='background: #eee;padding: 5px 10px ;'><strong>Area / Station : </strong> </td><td>" . $line2 . "</td></tr>";
+		$message .= "<tr><td style='background: #eee;padding: 5px 10px ;'><strong>Part Number : </strong> </td><td>" . $p_num . "</td></tr>";
+		$message .= "<tr><td style='background: #eee;padding: 5px 10px ;'><strong>Part Name : </strong> </td><td>" . $p_name . "</td></tr>";
+		$message .= "<tr><td style='background: #eee;padding: 5px 10px ;'><strong>Part Family : </strong> </td><td>" . $pf_name . "</td></tr>";
+		$message .= "<tr><td style='background: #eee;padding: 5px 10px ;'><strong>Reject Reason : </strong> </td><td>" . $fail_reason . "</td></tr>";
+		$message .= "<tr><td style='background: #eee;padding: 5px 10px ;'><strong>Quantity : </strong> </td><td>" . $quantity . "</td></tr>";
+		$message .= "<tr><td style='background: #eee;padding: 5px 10px ;'><strong>Operator/User : </strong> </td><td>" . $form_submitted_by . "</td></tr>";
+//    $message .= "<tr><td style='background: #eee;padding: 5px 10px ;'><strong>Number of items that are out of specification: </strong> </td><td>" . $temp_j . "</td></tr>";
+		$message .= "</table>";
+		$message .= "<br/>";
+		$message1 = "Please click on the following link to review the values that were uploaded : ";
+		$message2 = $siteURL . "material_tracability/view_material_mail.php?id=" . $material_trace_id;
+		$signature = "- USPL Process Control Team";
+
+
+		$structure = '<html><body>';
+		$structure .= "<br/><br/><span style='font-family: 'Source Sans Pro', sans-serif;color:#757575;font-weight:600;' > Hello,</span><br/><br/>";
+		$structure .= "<span style='font-family: 'Source Sans Pro', sans-serif;color:#757575;font-weight:600;' > " . $line1 . "</span><br/> ";
+		$structure .= "<span style='font-family: 'Source Sans Pro', sans-serif;color:#757575;font-weight:600;' > " . $message . "</span><br/> ";
+		$structure .= "<span style='font-family: 'Source Sans Pro', sans-serif;color:#757575;font-weight:600;' > " . $message1 . "</span><br/> ";
+		$structure .= "<span style='font-family: 'Source Sans Pro', sans-serif;color:#757575;font-weight:600;' > <a href=" . $message2 . ">View Form Data</a></span><br/> ";
+		$structure .= "<br/><br/>";
+		$structure .= $signature;
+		$structure .= "</body></html>";
+//	$mail->addAddress('ayesha@hematechservices.com', 'ayesha@hematechservices.com');
+		$mail->isHTML(true);
+		$mail->Subject = $subject;
+		$mail->Body = $structure;
+		$arr_out_of_tolerance_mail_list = explode(',', $out_of_tolerance_mail_list1);
+		foreach ($arr_out_of_tolerance_mail_list as $out_of_tolerance_mail_list) {
+			if ($out_of_tolerance_mail_list != "") {
+				$query0004 = sprintf("SELECT * FROM  sg_user_group where group_id = '$out_of_tolerance_mail_list' ");
+				$qur0004 = mysqli_query($db, $query0004);
+				while ($rowc0004 = mysqli_fetch_array($qur0004)) {
+					$u_name = $rowc0004['user_id'];
+					$query0005 = sprintf("SELECT * FROM  cam_users where users_id = '$u_name' ");
+					$qur0005 = mysqli_query($db, $query0005);
+					$rowc0005 = mysqli_fetch_array($qur0005);
+					$email = $rowc0005["email"];
+					$lasname = $rowc0005["lastname"];
+					$firstname = $rowc0005["firstname"];
+					$mail->addAddress($email, $firstname);
+				}
+				if (!$mail->send()) {
+					echo 'Mailer Error: ' . $mail->ErrorInfo;
+				} else {
+					$path = '{imap.gmail.com:993/imap/ssl}[Gmail]/Sent Mail';
+					$imapStream = imap_open($path, $mail->Username, $mail->Password);
+					$result = imap_append($imapStream, $path, $mail->getSentMIMEMessage());
+					imap_close($imapStream);
+				}
+			}
+		}
+	}
 }
 //header("Location:form_edit.php?id=$hidden_id");
 $page = "edit_material.php?id=$form_id";
