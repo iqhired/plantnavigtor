177f3fa01156652e565c37192d59c8aaadbf891e
swati-chavan1 <97093147+swati-chavan1@users.noreply.github.com>
2022-07-07 16:38:39 +0530

    change unlink command

--- 10x/delete_10x_image.php
+++ 10x/delete_10x_image.php
@@ -8,12 +8,10 @@ $sql1 = mysqli_query($db,$sql);
 $row = mysqli_fetch_array($sql1);
 $id = $row['10x_images_id'];
 $file_name = $row['image_name'];
+unlink("../assets/images/10x/".$time_stamp.'/'.$file_name);
 if(!is_dir($time_stamp)){
     unlink("../assets/images/10x/".$x_id.'/'.$file_name);
-}else{
-    unlink("../assets/images/10x/".$time_stamp.'/'.$file_name);
 }
-
       $sql = "DELETE FROM `10x_images` where 10x_images_id ='$x_id'";
 
 if (!mysqli_query($db, $sql)) {
