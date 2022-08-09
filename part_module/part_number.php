1e0118960933f88c71c110b63dc54b985e914e9d
swati-chavan1 <swati.chavan@iqhired.com>
2022-08-09 13:46:09 +0530

    shallow delete is not working

--- part_module/part_number.php
+++ part_module/part_number.php
@@ -511,18 +511,18 @@ include("../heading_banner.php");
                             </div>
 
 
-<!--							--><?php
-//							if (!empty($import_status_message)) {
-//								echo '<div class="alert ' . $message_stauts_class . '">' . $import_status_message . '</div>';
-//							}
-//							?>
-<!--							--><?php
-//							if (!empty($_SESSION[import_status_message])) {
-//								echo '<div class="alert ' . $_SESSION['message_stauts_class'] . '">' . $_SESSION['import_status_message'] . '</div>';
-//								$_SESSION['message_stauts_class'] = '';
-//								$_SESSION['import_status_message'] = '';
-//							}
-//							?>
+							<?php
+							if (!empty($import_status_message)) {
+								echo '<div class="alert ' . $message_stauts_class . '">' . $import_status_message . '</div>';
+							}
+							?>
+							<?php
+							if (!empty($_SESSION['import_status_message'])) {
+								echo '<div class="alert ' . $_SESSION['message_stauts_class'] . '">' . $_SESSION['import_status_message'] . '</div>';
+								$_SESSION['message_stauts_class'] = '';
+								$_SESSION['import_status_message'] = '';
+							}
+							?>
                         </div>
                         <div class="panel-footer p_footer">
                             <div class="col-md-4">
