4436e189e6ebdeb84712b2627449d0a3c6b2ec87
Asha M S <ashams001@gmail.com>
2022-08-22 16:30:23 +0530

    ipdated index

--- index.php
+++ index.php
@@ -102,6 +102,12 @@ if (!empty($_POST['user']) && !empty($_POST['pass']) ){
 		$message_stauts_class = $_SESSION["alert_danger_class"];
 		$import_status_message = $_SESSION["error_1"];
 	}
+	if($status == '1')
+	{
+		header("Location:change_password.php");
+	}else {
+		header("Location:line_status_grp_dashboard.php");
+	}
 	if ($pin_flag == "1") {
 		if ($pin == "0") {
 			$_SESSION['message_stauts_class'] = 'alert-danger';
@@ -111,12 +117,6 @@ if (!empty($_POST['user']) && !empty($_POST['pass']) ){
 			header("Location:line_status_grp_dashboard.php");
 		}
 	} else {
-		header("Location:line_status_grp_dashboard.php");
-	}
-	if($status == '1')
-	{
-		header("Location:change_password.php");
-	}else {
 		header("Location:line_status_grp_dashboard.php");
 	}
 }
