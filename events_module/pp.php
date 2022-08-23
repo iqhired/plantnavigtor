7c4900986190d232a04945f81c6fb68c85ce6aec
Asha M S <ashams001@gmail.com>
2022-08-23 15:57:54 +0530

    print logic

--- events_module/pp.php
+++ events_module/pp.php
@@ -2,6 +2,9 @@
 <html>
 <head>
     <META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
     <script src="https://testing.plantnavigator.com/assets/js/BrowserPrint.js"></script>
     <script src="https://testing.plantnavigator.com/assets/js/DevDemo.js"></script>
 </head>
