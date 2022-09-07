9df14d0177f879738ee99f57f78997644e14a392
Asha M S <ashams001@gmail.com>
2022-09-07 18:04:47 +0530

    schedular update

--- log_module/update_station_event_log_backend_schedular.php
+++ log_module/update_station_event_log_backend_schedular.php
@@ -14,7 +14,7 @@ $result_st = mysqli_query($db,$sql_st);
 $row_st =  mysqli_fetch_array($result_st);
 $station_event_old_id = $row_st['sg_station_event_old_id'];
 
-$sql0 = "SELECT * FROM sg_station_event_log where station_event_log_id > '$station_event_old_id' AND created_on <= '$chicagotime1'";
+$sql0 = "SELECT * FROM sg_station_event_log where station_event_log_id > '$station_event_old_id' AND created_on like '$chicagotime1%'";
 $result0 = mysqli_query($db, $sql0);
 
 while ($row = mysqli_fetch_array($result0)) {
