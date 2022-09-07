440932c7822c2d19516d1d8450143bacb9617dec
vidyaiq <vidya.ck@iqhired.com>
2022-09-07 18:37:07 +0530

    new file for rejection track

--- log_module/station_events_log.php
+++ log_module/station_events_log.php
@@ -7,23 +7,30 @@ $curdate = date('Y-m-d');
 $button = "";
 $temp = "";
 if (!isset($_SESSION['user'])) {
-	header('location: logout.php');
+    if($_SESSION['is_tab_user'] || $_SESSION['is_cell_login']){
+        header($redirect_tab_logout_path);
+    }else{
+        header($redirect_logout_path);
+    }
 }
-
-
 //Set the session duration for 10800 seconds - 3 hours
 $duration = $auto_logout_duration;
 //Read the request time of the user
 $time = $_SERVER['REQUEST_TIME'];
 //Check the user's session exist or not
 if (isset($_SESSION['LAST_ACTIVITY']) && ($time - $_SESSION['LAST_ACTIVITY']) > $duration) {
-	//Unset the session variables
-	session_unset();
-	//Destroy the session
-	session_destroy();
-	header($redirect_logout_path);
+    //Unset the session variables
+    session_unset();
+    //Destroy the session
+    session_destroy();
+    if($_SESSION['is_tab_user'] || $_SESSION['is_cell_login']){
+        header($redirect_tab_logout_path);
+    }else{
+        header($redirect_logout_path);
+    }
+
 //	header('location: ../logout.php');
-	exit;
+    exit;
 }
 //Set the time of the user's last activity
 $_SESSION['LAST_ACTIVITY'] = $time;
