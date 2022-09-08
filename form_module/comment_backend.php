1532d3ac0e8f613124168ba133f9846ec0eccfe1
vidyaiq <vidya.ck@iqhired.com>
2022-09-08 16:45:41 +0530

    new files of view rejection

---
+++ form_module/comment_backend.php
@@ -0,0 +1,15 @@
+<?php
+include("../config.php");
+$chicagotime = date("Y-m-d H:i:s");
+if (count($_POST) > 0) {
+    $message = $_POST['enter-message'];
+    $userid = $_POST['sender'];
+    if ($message != null) {
+        $sql1 = "INSERT INTO `comments`(`userid`, `message`,`comment_date`) VALUES ('$userid','$message','$chicagotime')";
+        if (!mysqli_query($db, $sql1)) {
+        } else {
+        }
+
+    }
+}
+?>
