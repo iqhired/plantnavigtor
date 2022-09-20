664751aa2c4723d9d6a4f884531b8d2184d06277
swati-chavan1 <swati.chavan@iqhired.com>
2022-09-20 12:24:53 +0530

    new page for link not mentioned in menu

---
+++ menu.php
@@ -0,0 +1,46 @@
+<?php include("config.php");
+$chicagotime = date("Y-m-d H:i:s");
+$temp = "";
+
+
+?>
+<h3>Spliting Records Through page_from to page_to </h3>
+<a href="<?php echo $siteURL ?>log_module/update_station_event_log_backend_page.php" target="_blank">
+    <h3><?php echo $siteURL ?>log_module/update_station_event_log_backend_page.php</h3>
+</a>
+<br>
+<h3>Schedular Link</h3>
+<a href="<?php echo $siteURL ?>log_module/update_station_event_log_backend_schedular.php" target="_blank">
+<h3><?php echo $siteURL ?>log_module/update_station_event_log_backend_schedular.php</h3>
+</a>
+<br>
+<h3>NPR Efficiency station based selection</h3>
+<a href="<?php echo $siteURL ?>config_module/npr_dashboard_station.php" target="_blank">
+    <h3><?php echo $siteURL ?>config_module/npr_dashboard_station.php</h3>
+</a>
+<br>
+<h3>NPR Efficiency each station based on Hour</h3>
+<a href="<?php echo $siteURL ?>config_module/npr_dashboard.php?id=5" target="_blank">
+    <h3><?php echo $siteURL ?>config_module/npr_dashboard.php?id=5</h3>
+</a>
+<br>
+<h3>Form Submit View Graph</h3>
+<a href="<?php echo $siteURL ?>form_module/form_submit_view.php" target="_blank">
+    <h3><?php echo $siteURL ?>form_module/form_submit_view.php</h3>
+</a>
+<br>
+<h3>View Form Rejection</h3>
+<a href="<?php echo $siteURL ?>form_module/form_rejection.php" target="_blank">
+    <h3><?php echo $siteURL ?>form_module/form_rejection.php</h3>
+</a>
+<br>
+<h3>Edit Form Rejection</h3>
+<a href="<?php echo $siteURL ?>form_module/edit_rejection.php" target="_blank">
+    <h3><?php echo $siteURL ?>form_module/edit_rejection.php</h3>
+</a>
+<br>
+<h3>View Form Rejection</h3>
+<a href="<?php echo $siteURL ?>form_module/view_rejetion.php" target="_blank">
+    <h3><?php echo $siteURL ?>form_module/view_rejetion.php</h3>
+</a>
+<br>
