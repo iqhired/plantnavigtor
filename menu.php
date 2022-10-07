de0809e5cd998e0ccf12f37ce09c060980a476d4
vidyaiq <vidya.ck@iqhired.com>
2022-10-07 09:41:20 +0530

    update menu

--- menu.php
+++ menu.php
@@ -12,11 +12,6 @@ $temp = "";
     <h3><?php echo $siteURL ?>config_module/form_sumbit_view_dashboard.php</h3>
 </a>
 <br>
-<h3>Daily/Monthly/Yearly Report By graph</h3>
-<a href="<?php echo $siteURL ?>config_module/form_submit_view_chart.php" target="_blank">
-    <h3><?php echo $siteURL ?>config_module/form_submit_view_chart.php</h3>
-</a>
-<br>
 <h3>Spliting Records Through page_from to page_to </h3>
 <a href="<?php echo $siteURL ?>log_module/update_station_event_log_backend_page.php" target="_blank">
     <h3><?php echo $siteURL ?>log_module/update_station_event_log_backend_page.php</h3>
