5bba423bfbbb159e84bab9d0ec20dbc2a1afd657
Asha M S <ashams001@gmail.com>
2022-11-03 18:57:22 +0530

    live DB credentials

--- config/database_config.php
+++ config/database_config.php
@@ -4,14 +4,14 @@ session_start();
 ini_set('display_errors', FALSE);
 
 $servername = "localhost";
-$username = "ashams001";
-$password = "iqHired@123";
-$dbname = "sg_crewAssignmentMgmt";
+$username = "u0txir940ctq3";
+$password = "8xtufivsxpu8";
+$dbname = "dbhnjuseplnng8";
 
 // to check whether pin is updated or not
 
-$db = mysqli_connect('localhost','ashams001','iqHired@123','sg_crewAssignmentMgmt');
-$mysqli = new mysqli('localhost', 'ashams001', 'iqHired@123', 'sg_crewAssignmentMgmt');
+$db = mysqli_connect('localhost','u0txir940ctq3','8xtufivsxpu8','dbhnjuseplnng8');
+$mysqli = new mysqli('localhost', 'u0txir940ctq3', '8xtufivsxpu8', 'dbhnjuseplnng8');
 
 //$db = mysqli_connect('localhost','sg_crew_assign_mgr','sg_crew_assign_mgr@2020','sg_crew_assign_mgmt');
 //$mysqli = new mysqli('localhost', 'sg_crew_assign_mgr', 'sg_crew_assign_mgr@2020', 'sg_crew_assign_mgmt');
@@ -20,7 +20,7 @@ date_default_timezone_set("America/chicago");
 
 $sitename = "SaarGummi";
 
-$scriptName = "http://plantnavigator:8888/";
-$siteURL = "http://plantnavigator:8888/";
+$scriptName = "https://plantnavigator.com/";
+$siteURL = "https://plantnavigator.com/";
 
 ?>
--- config/general_coding_config.php
+++ config/general_coding_config.php
@@ -2,20 +2,20 @@
 
 // to check whether pin is updated or not 
 $actual_link = "$_SERVER[REQUEST_URI]";
-$rest_api_uri = "http://restapi:8888/api/v1/";
+$rest_api_uri = "https://api.plantnavigator.com/api/v1/";
 if($actual_link != "/profile.php")
 {
-$pin = $_SESSION["pin"];
-$pin_flag = $_SESSION["pin_flag"];
+	$pin = $_SESSION["pin"];
+	$pin_flag = $_SESSION["pin_flag"];
 
-if($pin_flag == "1")
-{
-if($pin == "0"){
-	$_SESSION['message_stauts_class'] = 'alert-danger';
-	$_SESSION['import_status_message'] = 'Please Fill Pin';
-	header("Location:profile.php");
-}
-}
+	if($pin_flag == "1")
+	{
+		if($pin == "0"){
+			$_SESSION['message_stauts_class'] = 'alert-danger';
+			$_SESSION['import_status_message'] = 'Please Fill Pin';
+			header("Location:profile.php");
+		}
+	}
 }
 $auto_logout_duration = 10800;
 $redirect_logout_path = 'location:'. $siteURL . 'logout.php';
