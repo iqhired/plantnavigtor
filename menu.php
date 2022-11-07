02fdb0b9ac73e9fdb16f80406782e41ab9472c66
vidyaiq <vidya.ck@iqhired.com>
2022-11-07 09:42:15 +0530

    menu file edit based on conditions

--- menu.php
+++ menu.php
@@ -3,28 +3,23 @@ $chicagotime = date("Y-m-d H:i:s");
 $temp = "";
 ?>
 <h3>Scheduler Link</h3>
-<a href="<?php echo $siteURL ?>log_module/update_station_event_log_backend_schedular.php" target="_blank">
-    <h3><?php echo $siteURL ?>log_module/update_station_event_log_backend_schedular.php</h3>
+<a href="<?php echo $siteURL ?>log_module/se_log_schedular.php" target="_blank">
+    <h3><?php echo $siteURL ?>log_module/se_log_schedular.php</h3>
 </a>
 <br>
-<h3>NPR Data Scheduler Link</h3>
-<a href="<?php echo $siteURL ?>events_module/npr_data_store.php" target="_blank">
-    <h3><?php echo $siteURL ?>events_module/npr_data_store.php</h3>
+<h3>Assets Station Config</h3>
+<a href="<?php echo $siteURL ?>report_config_module/assets_config.php" target="_blank">
+    <h3><?php echo $siteURL ?>report_config_module/assets_config.php</h3>
+</a>
+<br>
+<h3>Edit Assets Station Config</h3>
+<a href="<?php echo $siteURL ?>report_config_module/edit_assets_config.php" target="_blank">
+    <h3><?php echo $siteURL ?>report_config_module/edit_assets_config.php</h3>
 </a>
 <br>
 <h3>Daily Efficiency Report Excel Generate</h3>
 <a href="<?php echo $siteURL ?>report_config_module/export_daily_report_gbp_excel.php" target="_blank">
     <h3><?php echo $siteURL ?>report_config_module/export_daily_report_gbp_excel.php</h3>
-</a>
-<br>
-<h3>Daily Efficiency Report Email Generate</h3>
-<a href="<?php echo $siteURL ?>report_config_module/export_daily_report_gbp_email.php" target="_blank">
-    <h3><?php echo $siteURL ?>report_config_module/export_daily_report_gbp_email.php</h3>
-</a>
-<br>
-<h3>Line Utilization Data By Date</h3>
-<a href="<?php echo $siteURL ?>config_module/line_utilization_view.php" target="_blank">
-    <h3><?php echo $siteURL ?>config_module/line_utilization_view.php</h3>
 </a>
 <br>
 <h3>Daily/Monthly/Yearly Report</h3>
