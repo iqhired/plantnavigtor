c2434dfe0af1ab3d70d9d1e4bbf120a63297f5b1
Asha M S <ashams001@gmail.com>
2022-11-07 15:41:33 +0530

    scanner code intergration QCode

--- qr.php
+++ qr.php
@@ -1,4 +1,13 @@
-<script src="./assets/js/html5-qrcode.min.js"></script>
+<!DOCTYPE html>
+<html>
+<head>
+    <META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
+    <script src="https://testing.plantnavigator.com/assets/js/html5-qrcode.min.js"></script>
+</head>
+<body>
 <div id="qr-reader" style="width: 600px"></div>
 <script>
     function onScanSuccess(decodedText, decodedResult) {
@@ -8,3 +17,5 @@
         "qr-reader", { fps: 10, qrbox: 250 });
     html5QrcodeScanner.render(onScanSuccess);
 </script>
+</body>
+</html>
