8ca5eb4d7286c2eee0a7911dc397ef9f7a9efd67
Asha M S <ashams001@gmail.com>
2022-11-09 16:12:14 +0530

    schedular bug fix

--- log_module/se_log_schedular.php
+++ log_module/se_log_schedular.php
@@ -132,10 +132,9 @@ while ($row_st_202 = mysqli_fetch_array($result_st_202)) {
 						$next_date = $crea_date[0];
 						if($z > 2){
 							$loop_tot_time = $loop_tot_time - $rem_day_time - (24 * ($z - 2));
-						}else{
-							$loop_tot_time = $loop_tot_time - $rem_day_time;
 						}
-						if ($loop_tot_time > 24) {
+						$cr_time = $start_time + $loop_tot_time;
+						if ($cr_time > 24) {
 							$end_time2 = $crea_date[0] . ' ' . '23:59:59';
 							$sql_up = "update sg_station_event_log_update set total_time = '$rem_day_time' , end_time = '$end_time2' where day_seq = '$z' AND station_event_id = '$station_event_id' and  sg_station_event_old_id = '$station_event_log_id'";
 							$result_up = mysqli_query($db, $sql_up);
