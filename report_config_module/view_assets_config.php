d415535861109cd0eb4db22beb7b5b761184c92d
swati-chavan1 <swati.chavan@iqhired.com>
2022-11-09 18:30:51 +0530

    changes in station assets

--- report_config_module/view_assets_config.php
+++ report_config_module/view_assets_config.php
@@ -51,7 +51,7 @@ $idddd = preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>
-        <?php echo $sitename; ?> | View 10x</title>
+        <?php echo $sitename; ?> | View Asset Config</title>
     <!-- Global stylesheets -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet"
           type="text/css">
