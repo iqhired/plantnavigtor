31b48a84dc807730243c7fc6e8eb1f25d7f8cc8d
Asha M S <ashams001@gmail.com>
2022-11-16 19:31:02 +0530

    new NPR Dashboard Code revert to single update from loop

--- events_module/npr_schedular.php
+++ events_module/npr_schedular.php
@@ -15,34 +15,27 @@ $result_st_202 = mysqli_query($db, $sql_st_202);
 
 while ($row_st_202 = mysqli_fetch_array($result_st_202)) {
 	$ll_id = $row_st_202['line_id'];
-	$counter = $time;
-	while($counter > 0){
-		$t1 = $counter;
-		$t2 = $counter + 1;
-		$sql = "SELECT SUM(good_pieces) as good_pieces,SUM(bad_pieces) AS bad_pieces,SUM(rework) as rework , good_bad_pieces_details.station_event_id as sei FROM `good_bad_pieces_details`  INNER JOIN sg_station_event ON good_bad_pieces_details.station_event_id = sg_station_event.station_event_id where 1  and sg_station_event.line_id = '$ll_id' and date(`created_at`) >= '$chicagotime_from_date' and date(`created_at`) <= '$chicagotime_to_date' and hour(good_bad_pieces_details.created_at) >= '$t1'  and hour(good_bad_pieces_details.created_at) < '$t2'";
-		$result1 = mysqli_query($db, $sql);
-		$rowc = mysqli_fetch_array($result1);
-		$gp = $rowc['good_pieces'];
-		$bp = $rowc['bad_pieces'];
-		$rwp = $rowc['rework'];
-		$station_event_id = $rowc['sei'];
-		$gr = $gp + $rwp;
-		$npr_data = $t1 . ":" . $gr . ":" . $bp;
+	$sql = "SELECT SUM(good_pieces) as good_pieces,SUM(bad_pieces) AS bad_pieces,SUM(rework) as rework , good_bad_pieces_details.station_event_id as sei FROM `good_bad_pieces_details`  INNER JOIN sg_station_event ON good_bad_pieces_details.station_event_id = sg_station_event.station_event_id where 1  and sg_station_event.line_id = '$ll_id' and date(`created_at`) >= '$chicagotime_from_date' and date(`created_at`) <= '$chicagotime_to_date' and hour(good_bad_pieces_details.created_at) >= '$time'  and hour(good_bad_pieces_details.created_at) < '$oneHourLater'";
+	$result1 = mysqli_query($db, $sql);
+	$rowc = mysqli_fetch_array($result1);
+	$gp = $rowc['good_pieces'];
+	$bp = $rowc['bad_pieces'];
+	$rwp = $rowc['rework'];
+	$station_event_id = $rowc['sei'];
+	$gr = $gp + $rwp;
+	$npr_data = $time . ":" . $gr . ":" . $bp;
 
-		$sql_st_2021 = "SELECT count(*) as r_count FROM npr_station_data  where line_id = '$ll_id' and npr_h = '$t1' and date(created_on) = '$chicagotime_to_date'";
-		$result_st_2021 = mysqli_query($db, $sql_st_2021);
-		$rowc_2021 = mysqli_fetch_array($result_st_2021);
-		$r_count  = $rowc_2021['r_count'];
-		if(($r_count == 0) && ((!empty($gr) && ($gr > 0) ) || (!empty($bp) && ($bp >0)))){
-			$sqlt = "INSERT INTO `npr_station_data`(`line_id`,`npr_data`,`npr_h`, `npr_gr`, `npr_b`,`created_on`) VALUES ('$ll_id','$npr_data','$t1','$gr','$bp','$chicagotime')";
-			$resultt = mysqli_query($db, $sqlt);
-		}else{
-			$sqlt = "update `npr_station_data` set npr_data = '$npr_data' , `npr_gr` = '$gr', `npr_b` = '$bp' ,`created_on` = '$chicagotime'  where line_id = '$ll_id' and npr_h = '$t1' and date(created_on) = '$chicagotime_to_date' ";
-			$resultt = mysqli_query($db, $sqlt);
-		}
-		$counter-- ;
+	$sql_st_2021 = "SELECT count(*) as r_count FROM npr_station_data  where line_id = '$ll_id' and npr_h = '$time' and date(created_on) = '$chicagotime_to_date'";
+	$result_st_2021 = mysqli_query($db, $sql_st_2021);
+	$rowc_2021 = mysqli_fetch_array($result_st_2021);
+	$r_count  = $rowc_2021['r_count'];
+	if(($r_count == 0) && ((!empty($gr) && ($gr > 0) ) || (!empty($bp) && ($bp >0)))){
+		$sqlt = "INSERT INTO `npr_station_data`(`line_id`,`npr_data`,`npr_h`, `npr_gr`, `npr_b`,`created_on`) VALUES ('$ll_id','$npr_data','$time','$gr','$bp','$chicagotime')";
+		$resultt = mysqli_query($db, $sqlt);
+	}else{
+		$sqlt = "update `npr_station_data` set npr_data = '$npr_data' , `npr_gr` = '$gr', `npr_b` = '$bp' ,`created_on` = '$chicagotime'  where line_id = '$ll_id' and npr_h = '$time' and date(created_on) = '$chicagotime_to_date' ";
+		$resultt = mysqli_query($db, $sqlt);
 	}
-
 
 }
 ?>
