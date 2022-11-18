5f930957d31377e2b4fc06d822a9439817c8ebb4
vidyaiq <vidya.ck@iqhired.com>
2022-11-18 16:58:54 +0530

    changes done

--- report_config_module/export_first_piece_sheet_lab_excel.php
+++ report_config_module/export_first_piece_sheet_lab_excel.php
@@ -1,7 +1,5 @@
 <?php
 ob_start();
-//ini_set('display_errors', 'off');
-//session_start();
 include '../config.php';
 $chicagotime = date('m-d-Y', strtotime('-1 days'));
 //$chicagotime1 = date('Y-m-d', strtotime('-1 days'));
