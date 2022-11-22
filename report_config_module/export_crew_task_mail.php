7f2c1519e723c44347476cd20f627af13356a4a8
vidyaiq <vidya.ck@iqhired.com>
2022-11-22 18:02:02 +0530

    email configuration done by these files

--- report_config_module/export_crew_task_mail.php
+++ report_config_module/export_crew_task_mail.php
@@ -4,23 +4,12 @@ include("../config.php");
 $chicagotime = date('d-m-Y', strtotime('-1 days'));
 $tboardName =''; 
 $subject = "Daily Mail Report";
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
 require '../vendor/autoload.php';
+include ("../email_config.php");
 //main query
 $mainquery = sprintf("SELECT * FROM  tm_task_log_config ");
 $mainqur = mysqli_query($db, $mainquery);
 while ($mainrowc = mysqli_fetch_array($mainqur)) {
-    $mail = new PHPMailer();
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->Port = 587;
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->SMTPAuth = true;
-    $mail->Username = 'admin@plantnavigator.com';
-    $mail->Password = 'S@@rgummi_2022';
-    $mail->setFrom('admin@plantnavigator.com', 'admin@plantnavigator.com');
     $taskboard_id = $mainrowc["taskboard"];
 // taskboard name
     $namequr = mysqli_query($db, "SELECT * FROM `sg_taskboard` where sg_taskboard_id = '$taskboard_id' ");
