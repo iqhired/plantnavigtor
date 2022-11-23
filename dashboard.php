711570e2dc7956f51f196847163b2763d155052a
vidyaiq <vidya.ck@iqhired.com>
2022-11-23 17:19:03 +0530

    bug fix

--- dashboard.php
+++ dashboard.php
@@ -184,6 +184,7 @@ $message = date("Y-m-d H:i:s");
                                         if ($variable123 != "") {
                                             ?>
                                             <script>
+                                                var now = calcTime('Chicago', '-6');
                                                 // Set the date we're counting down to
                                                 var iddd<?php echo $countervariable; ?> = $("#id<?php echo $countervariable; ?>").val();
                                                 console.log(iddd<?php echo $countervariable; ?>);
@@ -191,6 +192,7 @@ $message = date("Y-m-d H:i:s");
                                                 // Update the count down every 1 second
                                                 var x = setInterval(function () {
                                                     // Get today's date and time
+                                                    var now = calcTime('Chicago', '-6');
                                                     //new Date().getTime();
                                                     // Find the distance between now and the count down date
                                                     var distance = now - countDownDate<?php echo $countervariable; ?>;
