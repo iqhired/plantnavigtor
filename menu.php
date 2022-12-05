5ac9cdc76697fc84c685bbbb02afc25894ae4932
vidyaiq <vidya.ck@iqhired.com>
2022-12-05 12:10:06 +0530

    updated menu and added form module

--- menu.php
+++ menu.php
@@ -7,14 +7,9 @@ $temp = "";
     <h3><?php echo $siteURL ?>log_module/se_log_schedular.php</h3>
 </a>
 <br>
-<h3>Station Dashboard</h3>
-<a href="<?php echo $siteURL ?>config_module/station_dashboard.php" target="_blank">
-    <h3><?php echo $siteURL ?>config_module/station_dashboard.php</h3>
-</a>
-<br>
-<h3>Daily/Monthly/Yearly Report</h3>
-<a href="<?php echo $siteURL ?>config_module/form_sumbit_view_dashboard.php" target="_blank">
-    <h3><?php echo $siteURL ?>config_module/form_sumbit_view_dashboard.php</h3>
+<h3>SPC Analytics graph </h3>
+<a href="<?php echo $siteURL ?>form_module/form_graph.php" target="_blank">
+    <h3><?php echo $siteURL ?>form_module/form_graph.php</h3>
 </a>
 <br>
 <h3>Spliting Records Through page_from to page_to </h3>
@@ -22,8 +17,46 @@ $temp = "";
     <h3><?php echo $siteURL ?>log_module/update_station_event_log_backend_page.php</h3>
 </a>
 <br>
-<h3>Graph Link for line graph </h3>
-<a href="<?php echo $siteURL ?>form_module/form_graph.php" target="_blank">
-    <h3><?php echo $siteURL ?>form_module/form_graph.php</h3>
+<h3>Daily/Monthly/Yearly Report</h3>
+<a href="<?php echo $siteURL ?>config_module/form_sumbit_view_dashboard.php" target="_blank">
+    <h3><?php echo $siteURL ?>config_module/form_sumbit_view_dashboard.php</h3>
 </a>
 <br>
+
+/****  Form Module Links ****/
+<h3>Form Create </h3>
+<a href="<?php echo $siteURL ?>form_module/form_settings.php" target="_blank">
+    <h3><?php echo $siteURL ?>form_module/form_settings.php</h3>
+</a>
+<br>
+<h3>Form Edit </h3>
+<a href="<?php echo $siteURL ?>form_module/form_edit.php" target="_blank">
+    <h3><?php echo $siteURL ?>form_module/form_edit.php</h3>
+</a>
+<br>
+<h3>Form Submit </h3>
+<a href="<?php echo $siteURL ?>form_module/user_form.php" target="_blank">
+    <h3><?php echo $siteURL ?>form_module/user_form.php</h3>
+</a>
+<br>
+<h3>Form View </h3>
+<a href="<?php echo $siteURL ?>form_module/submit_user_form.php" target="_blank">
+    <h3><?php echo $siteURL ?>form_module/submit_user_form.php</h3>
+</a>
+<br>
+<a href="<?php echo $siteURL ?>form_module/view_submit.php" target="_blank">
+    <h3><?php echo $siteURL ?>form_module/view_submit.php</h3>
+</a>
+<br>
+<a href="<?php echo $siteURL ?>form_module/view_user_form_data.php" target="_blank">
+    <h3><?php echo $siteURL ?>form_module/view_user_form_data.php</h3>
+</a>
+<br>
+<a href="<?php echo $siteURL ?>form_module/view_form_data.php" target="_blank">
+    <h3><?php echo $siteURL ?>form_module/view_form_data.php</h3>
+</a>
+<br>
+<h3>Form Rejection</h3>
+<a href="<?php echo $siteURL ?>form_module/edit_rejection.php" target="_blank">
+    <h3><?php echo $siteURL ?>form_module/edit_rejection.php</h3>
+</a>
