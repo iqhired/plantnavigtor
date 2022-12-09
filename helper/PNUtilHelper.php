d76835b6156a4c6ddb6958e4de63c8c963d3b062
Asha M S <ashams001@gmail.com>
2022-12-09 17:01:46 +0530

    updated helper class

--- helper/PNUtilHelper.php
+++ helper/PNUtilHelper.php
@@ -10,4 +10,20 @@
 function dateReadFormat($datetime) {
 	return date("d-M-Y H:i:s" , strtotime($datetime));
 }
+/**
+ * This methos return the SQL date in readable format
+ * @param $datetime
+ * @return false|string
+ */
+function onlydateReadFormat($datetime) {
+	return date("d-M-Y" , strtotime($datetime));
+}
+/**
+ * This methos return the SQL date in readable format
+ * @param $datetime
+ * @return false|string
+ */
+function datemdY($datetime) {
+	return date("m-d-Y" , strtotime($datetime));
+}
 ?>
