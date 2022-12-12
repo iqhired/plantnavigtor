6cb6bd44f5e4b618f5fe1face095958fe8568d28
vidyaiq <vidya.ck@iqhired.com>
2022-12-12 09:59:11 +0530

    change style

--- admin_menu.php
+++ admin_menu.php
@@ -1331,7 +1331,7 @@ $msg = explode(',', $msg); ?>
                     <li class="nav-item" id="ic">
                         <a class="nav-link">Supplier
                             <svg xmlns="http://www.w3.org/2000/svg" class="arrow" viewBox="0 0 20 20"
-                                 style="margin-left: 115px;">
+                                 style="margin-left: 65px;">
                                 <path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"/>
                             </svg>
                         </a>
