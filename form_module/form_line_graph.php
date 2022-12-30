e3c4ad2ce207a225fe671433804d35522c29f12d
vidyaiq <vidya.ck@iqhired.com>
2022-12-30 18:07:27 +0530

    new files of spc analytics

--- form_module/form_line_graph.php
+++ form_module/form_line_graph.php
@@ -1,9 +1,9 @@
 <?php include("../s_config.php");
 include("../config.php");
 $form_create_id = $_POST['form_create'];
-$form_item_id = $_POST['form_item_id'];
 $date_from = $_POST['date_from'];
 $date_to = $_POST['date_to'];
+$form_item_id = $_POST['form_item_id'];
 error_log("inside Form Line graph");
     //$sql = "SELECT data_item_value FROM `spc_schedular_data` WHERE `data_item_id` = '$form_item_id' and created_at >= '$date_from' and created_at <= '$date_to'";
 $sql = "SELECT data_item_value as data_item_value , data_item_desc as data_item_desc , created_at as created_at FROM `spc_schedular_data` WHERE `data_item_id` = '$form_item_id'";
@@ -28,6 +28,3 @@ $sql = "SELECT data_item_value as data_item_value , data_item_desc as data_item_
     }
     $response['posts'] = $posts;
     echo json_encode($response);
-
-
-
